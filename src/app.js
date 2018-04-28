fc1b829b3054b611c422c4eda7091299641e9e2c
WeiWei <838286494@qq.com>
2018-04-28 14:04:52 +0800

    just start

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import { React, Component } from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                hello react!
+            </div>
+        )
+    }
+}
+export default App
