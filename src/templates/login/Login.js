b7fe63bc4a5243c1ed7cbf1535b43c6350d9466b
WeiWei <838286494@qq.com>
2018-05-11 13:31:00 +0800

    小改动

--- src/templates/login/Login.js
+++ src/templates/login/Login.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import { Row, Col, Input } from "antd";
+import { Form, Row, Col, Input } from "antd";
+import { Button, Radio, DatePicker,TimePicker, message } from 'antd';
 import "../../css/login.css";
 
+const FormItem = Form.Item;
+
 class Login extends React.Component{
+    handleLoginClick(e){
+        let xmlhttp;
+        xmlhttp=new XMLHttpRequest();
+        xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                let responseObj = JSON.parse(xmlhttp.responseText);
+                /*
+                处理response
+                */
+            }
+        }.bind(this)
+        xmlhttp.open("POST","url",false);
+        xmlhttp.setRequestHeader("Content-Type","application/json");
+        xmlhttp.send();
+    }
     render(){
         const clientHeight = document.body.clientHeight;
+        const formStyle = {
+            width: 800,
+            marginLeft: "auto",
+            marginRight: "auto",
+        }
         return(
             <div className="loginContent" style={{height: clientHeight}}>
-                <div className="loginModal">
-                    <div className="loginForm">
-                        <Row>
-                            <Col span={4}><span>账号：</span></Col>
-                            <Col span={20}><Input /></Col>
-                        </Row>
-                        <Row>
-                            <Col span={4}><span>密码：</span></Col>
-                            <Col span={20}><Input /></Col>
-                        </Row>
-                    </div>
-                </div>
-                {/*<img src="./src/img/background.png" width="100%" height={clientHeight}/>*/}
+                <Form style={formStyle}>
+                    <FormItem
+                        label="账号"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 14 }}
+                    >
+                        <Input />
+                    </FormItem>
+                    <FormItem
+                        label="密码"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 14 }}
+                    >
+                        <Input />
+                    </FormItem>
+                    <FormItem wrapperCol={{ span: 16, offset: 6 }} style={{ marginTop: 24 }}>
+                        <Button type="primary">确定</Button>
+                    </FormItem>
+                </Form>
             </div>
         )
     }
